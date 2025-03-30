59f7bec884593fe2e3f7c8ac1da519ca93b290dd
Emilbek <emilbek@macbookair.home>
2025-03-30 00:01:35 -0400

    Refactor: Renamed App.js to MainComponent.js and updated imports

--- src/MainComponent.js
+++ src/MainComponent.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My first React app
         </a>
       </header>
     </div>
